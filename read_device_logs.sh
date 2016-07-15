fcbb323c1a795c5f386fb81988adb7c77e9a8767
Daniel Rodríguez <webmaster@hotfixed.net>
2016-07-14 20:55:57 -0500

    Create read_device_logs.sh

---
+++ read_device_logs.sh
@@ -0,0 +1,13 @@
+RANGE=10
+SCRIPTPATH=$( cd $(dirname $0); pwd -P )
+while true
+do
+    while read line
+    do
+        number=$RANDOM;
+        let "number %= $RANGE";
+        sleep $number;
+        echo "$line";
+        logger "$line";
+    done < $SCRIPTPATH/device.log
+done
